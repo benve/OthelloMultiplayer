e2197dd4f259a30930e7e1161a1a3b96e38f647a
Giacomo Benvenuti <giacomo.benvenuti@gmail.com>
2011-04-16 19:41:18 +0200

    Creazione classe del nodo

--- src/main/java/com/github/benve/othellomultiplayer/network/Node.java
+++ src/main/java/com/github/benve/othellomultiplayer/network/Node.java
@@ -1,9 +1,43 @@
 package com.github.benve.othellomultiplayer.network;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  *
  * @author Giacomo Benvenuti
  */
 public class Node implements RemoteNode {
-    
+
+    private RemoteNode next;
+
+    public void setNext(String nextIP, int nextPort) throws RemoteException, NotBoundException {
+        // http://java.sun.com/docs/books/tutorial/rmi/overview.html
+        Registry registry = LocateRegistry.getRegistry(nextIP, nextPort);
+        this.next = (RemoteNode) registry.lookup("Node");
+    }
+
+    /**
+     *
+     * @param lport Porta su cui sta in ascolto il nodo
+     */
+    public Node(int lport) throws RemoteException, AlreadyBoundException {
+        RemoteNode node = (RemoteNode) UnicastRemoteObject.exportObject(this, 0);
+        Registry registry = LocateRegistry.createRegistry(lport);
+        registry.bind("Node", node);
+
+    }
+
+
+    public void msg(String s) {
+        System.out.println(s);
+
+    }
+
+
+
 }
--- src/main/java/com/github/benve/othellomultiplayer/network/RemoteNode.java
+++ src/main/java/com/github/benve/othellomultiplayer/network/RemoteNode.java
@@ -1,11 +1,15 @@
 package com.github.benve.othellomultiplayer.network;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  *
  * @author Giacomo Benvenuti
  */
 public interface RemoteNode extends Remote {
+
+    public void msg(String s) throws RemoteException;
+
     
 }
