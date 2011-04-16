50dd0323d4dd02bbf5d7cf22879e892d89f4310e
Giacomo Benvenuti <giacomo.benvenuti@gmail.com>
2011-04-16 12:39:49 +0200

    corretto package

---
+++ src/main/java/com/github/benve/othellomultiplayer/PlayerInterface.java
@@ -0,0 +1,17 @@
+package com.github.benve.othellomultiplayer;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ledonne
+ */
+public interface PlayerInterface {
+    public String getName();
+    public String getUUID();
+    public String getUrl();
+    public String getPort();
+}
