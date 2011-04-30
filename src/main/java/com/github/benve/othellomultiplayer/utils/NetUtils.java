750e16a49c8ec81efb01e3001a44c8bd3548ef00
Giacomo Benvenuti <giacomo.benvenuti@gmail.com>
2011-04-30 09:55:06 +0200

    Aggiunta classe di utilità per ottenere l'indirizzo locale

---
+++ src/main/java/com/github/benve/othellomultiplayer/utils/NetUtils.java
@@ -0,0 +1,36 @@
+package com.github.benve.othellomultiplayer.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Giacomo Benvenuti
+ * Date: 4/30/11
+ * Time: 9:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NetUtils {
+
+    /**
+     * Ritorna un indirizzo ip pubblico della macchina
+     * @return un indirizzo pubblico della macchina, null ne la macchina non ne ha
+     * @throws UnknownHostException
+     */
+    public static InetAddress getPublicIP() throws UnknownHostException {
+
+        //Ottengo tutti gli IP della macchina
+        String hostName = InetAddress.getLocalHost().getHostName();
+        InetAddress addrs[] = InetAddress.getAllByName(hostName);
+
+        for (InetAddress addr : addrs) {
+            if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress()) {
+                return addr;
+            }
+        }
+
+        return null;
+    }
+
+
+}
