5260f7960715d80d24fcb1644afe859f7390b045
Giacomo Benvenuti <giacomo.benvenuti@gmail.com>
2011-05-29 19:22:18 +0200

    Metodo per ottenere successivo nell'anello

--- src/main/java/com/github/benve/othellomultiplayer/game/PlayerList.java
+++ src/main/java/com/github/benve/othellomultiplayer/game/PlayerList.java
@@ -13,6 +13,24 @@ import java.util.List;
  */
 public class PlayerList extends LinkedList<Player> implements List<Player>{
 
+    /**
+     * Dato un giocatore ritorna il next della lista
+     * @param myP giocatore corrente
+     * @return giocatore successivo, null se non trovo il giocatore corrente
+     */
+    public Player nextP(Player myP) {
 
+        for (int i = 0; i < (this.size()-1); i++) {//Scorro tutti tranne l'ultimo
+            if (this.get(i).getUuid() == myP.getUuid()) {
+                return this.get(i+1);
+            }
+        }
+        //Se sono l'ultimo torno in testa alla lista
+        if (this.get(this.size()-1).getUuid() == myP.getUuid()) {
+            return this.get(0);
+        }
+
+        return null;
+    }
 
 }
